9c3dc6df345da7fcc3b0ef0bf6a8f48c98e47c7f
ckhosh <40801322+ckhosh@users.noreply.github.com>
2022-02-26 18:35:29 -0600

    Debugging for google assistant actions

---
+++ WastePickupAPI/Controllers/DebugController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WastePickupAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DebugController : ControllerBase
+    {
+        [HttpPost("")]
+        public async Task<ActionResult<bool>> AnyTypeOfGetCall([FromBody] Object anyObject)
+        {
+            Console.WriteLine("BlankAction Called");
+            Console.WriteLine(anyObject.ToString());
+            return Ok(true);
+        }
+
+        [HttpPost("{anyAction}")]
+        public async Task<ActionResult<bool>> AnyTypeOfGetCalled(string anyAction,[FromBody] Object anyObject)
+        {
+            Console.WriteLine("AnyAction Called");
+            Console.WriteLine(anyObject.ToString());
+            return Ok(true);
+        }
+    }
+}
